287ef1d1027cac764d0ecde17211c5814ee4697f
Ubuntu <ubuntu@ip-172-31-37-4.us-west-2.compute.internal>
2017-04-26 15:58:07 +0000

    added checkPassword function in pgnode module

--- pgnode.js
+++ pgnode.js
@@ -1,15 +1,30 @@
 // Connector to T2 DB for bot
 
 var pg = require('pg')
+var bcrypt = require('bcrypt')
 
 process.on('unhandledRejection', function(e) {
   console.log(e.message, e.stack)
 })
 
-var pool = new pg.Pool('postgres://tgrm:lkilkzavr@localhost:5432/tgrm_db')
-
-var query = pool.query('SELECT password_hash FROM system_user')
-
-query.on('row', function(e,r) {
-	console.log(row[0])
+var pool = new pg.Pool(
+	{
+  user: 'tgrm',
+  password: 'lkilkzavr',
+  host: 'localhost',
+  database: 'tgrm_db',
+  max: 10, // max number of clients in pool
+  idleTimeoutMillis: 2000, // close & remove clients which have been idle > 1 second
 })
+
+function checkPassword(login,password) {
+	var password_query = pool.query('SELECT password_hash FROM system_user WHERE login=$1',[login]);
+	var res = new Promise((resolve,reject)=> {
+	password_query.then(function(val){
+		resolve(bcrypt.compare(password,val.rows[0].password_hash))
+	}).catch(function(e){resolve(false);});
+	})
+	return res;
+}
+
+module.exports.checkPassword=checkPassword;
